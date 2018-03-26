8232c549c1394ad0e1b41e96ca0e7e66413f79ed
chaibluesky37 <chaibluesky37@gmail.com>
2018-03-26 23:06:40 +0700

    v1.0.3

--- App.js
+++ App.js
@@ -11,12 +11,13 @@ import Firebase from 'firebase';
 export default class App extends Component {
     componentWillMount() {
         Firebase.initializeApp({
-            apiKey: "your firebase",
-            authDomain: "your firebase",
-            databaseURL: "your firebase",
-             projectId: "your firebase",
-            storageBucket: "your firebase",
-            messagingSenderId: "your firebase"
+            apiKey: "AIzaSyDwj2DN9HBan5nUkdGJihorBG2eMZW-Kvc",
+            authDomain: "kmutnbpj3700.firebaseapp.com",
+            databaseURL: "https://kmutnbpj3700.firebaseio.com",
+             projectId: "kmutnbpj3700",
+            storageBucket: "kmutnbpj3700.appspot.com",
+            messagingSenderId: "298972134522"
+
         })
     }
     render() {
